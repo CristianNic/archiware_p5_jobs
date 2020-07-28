3ab11277ad1119ed47fd5f8b5af86855b22da6d6
CristianNic <cristian.niculescu@gmail.com>
2020-07-27 23:18:30 -0700

    wrap text along column

--- js/archiware_p5_jobs.js
+++ js/archiware_p5_jobs.js
@@ -1,10 +1,14 @@
 
 
-var formatNewLines = function ( nRow ) {
-  var celldata=$('td:eq(2)', nRow).text();
-  $('td:eq(2)', nRow).html(celldata.replace(/\n/g, "<br>").replace(/\r/g, "<br>"));
+var formatNewLines = function ( col, row ) {
+  // Get cell data and cell position, and make it into variables `cell` and `value`
+  alert("here");
+  var cell = $('td:eq('+col+')', row);
+  var value = cell.text();
+  // Process contents of variable `value`
+  var newValue = value.replace(/\n/g, "<br>").replace(/\r/g, "<br>");
+  // Output new variable `value` to cell
+  cell.html(newValue)
 }
-
-
 
 
