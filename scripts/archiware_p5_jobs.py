3cf625e45795799df3979b8f3faf89524fe0305b
CristianNic <cristian.niculescu@gmail.com>
2020-08-01 23:21:05 -0700

    run all job numbers

--- scripts/archiware_p5_jobs.py
+++ scripts/archiware_p5_jobs.py
@@ -6,29 +6,49 @@ import subprocess
 import collections
 import json
 
-def nsdchat_check():
+def nsdchat_job_check():
 
-    cmds = ['sh nsdchat_job_simulator.sh Job warning',
-            'sh nsdchat_job_simulator.sh Job 12267 describe',
-            'sh nsdchat_job_simulator.sh Job 12438 describe']
+    cmd = ['sh nsdchat_job_simulator.sh Job 2_warnings'] # cmd requires full .sh file path name
 
-    out = []
+    proc = subprocess.Popen(cmd, text=True, shell=True,
+                        stdin=subprocess.PIPE,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
+    output, err = proc.communicate()
+    # print(output) # >> 12267 12421
 
+    # convert output string into a list
+    out = output.split(' ')
+    # print(out) # >> ['12267', '12421']
+
+    # create new cmds list from output. for each add path + descibe or xmlticket
+    cmds = []
+
+    for i in out:
+        describe = 'sh nsdchat_job_simulator.sh Job ' + i + ' describe'  # >> 'sh nsdchat_job_simulator.sh Job 12267 descride'
+        xmlpath  = 'sh nsdchat_job_simulator.sh Job ' + i + ' xmlticket'  # >> 'sh nsdchat_job_simulator.sh Job 12267 xmlpath'
+        cmds.append(describe)
+        cmds.append(xmlpath)
+    print(cmds) # >> ['sh nsdchat_job_simulator.sh Job 12267 descride', 'sh nsdchat_job_simulator.sh Job 12267 xmlpath',
+                #    'sh nsdchat_job_simulator.sh Job 12421 descride', 'sh nsdchat_job_simulator.sh Job 12421 xmlpath']
+
+    # run the new list of commands
     for cmd in cmds:
         proc = subprocess.Popen(cmd, text=True, shell=True,
                             stdin=subprocess.PIPE,
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
         output, err = proc.communicate()
-        out.append(output)
-        print(output)
-        # >> 12267 12284 12294 12438 12421 12413
-        # >> Sync Plan: Sync Plan 1
-        # >> Sync Plan: Sync Plan 1
-
-    print(out)
-        # >> ['12267 12284 12294 12438 12421 12413', 'Sync Plan: Sync Plan 1', 'Sync Plan: Sync Plan 1']
-
+        print(output) # >> Sync Plan: Sync Plan 1
+                      # >> <jobreport>
+                      # >>      <description>Sync Plan: Sync Plan 1</description>
+                      # >>      <startdate>15.05.2016</startdate>
+                      # >>      <enddate>15.05.2016</enddate>
+                      # >>      <starttime>00:00:00</starttime>
+                      # >> ...
+                      # >> Sync Plan: Sync Plan 2
+                      # >>      <jobreport>
+                      # >> ...
 
 def two_dicts():
     out = []
@@ -50,7 +70,7 @@ def main():
 
     # Get information about Archiware P5 Jobs
     # result = two_dicts()
-    result = nsdchat_check()
+    result = nsdchat_job_check()
 
     cachedir = '%s/cache' % os.path.dirname(os.path.realpath(__file__))
     with open(os.path.join(cachedir, 'archiware_p5_jobs.json'), 'w') as fp:
