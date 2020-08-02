a4a8c9862359de79bf1dcdf2497da4f1dedbe192
CristianNic <cristian.niculescu@gmail.com>
2020-08-01 23:14:47 -0700

    capture terminal output

--- scripts/archiware_p5_jobs.py
+++ scripts/archiware_p5_jobs.py
@@ -6,6 +6,30 @@ import subprocess
 import collections
 import json
 
+def nsdchat_check():
+
+    cmds = ['sh nsdchat_job_simulator.sh Job warning',
+            'sh nsdchat_job_simulator.sh Job 12267 describe',
+            'sh nsdchat_job_simulator.sh Job 12438 describe']
+
+    out = []
+
+    for cmd in cmds:
+        proc = subprocess.Popen(cmd, text=True, shell=True,
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+        output, err = proc.communicate()
+        out.append(output)
+        print(output)
+        # >> 12267 12284 12294 12438 12421 12413
+        # >> Sync Plan: Sync Plan 1
+        # >> Sync Plan: Sync Plan 1
+
+    print(out)
+        # >> ['12267 12284 12294 12438 12421 12413', 'Sync Plan: Sync Plan 1', 'Sync Plan: Sync Plan 1']
+
+
 def two_dicts():
     out = []
     job1 = {"job_number" : "12685 \n Success"}
@@ -25,8 +49,8 @@ def main():
         exit(0)
 
     # Get information about Archiware P5 Jobs
-    result = dict()
-    result = two_dicts()
+    # result = two_dicts()
+    result = nsdchat_check()
 
     cachedir = '%s/cache' % os.path.dirname(os.path.realpath(__file__))
     with open(os.path.join(cachedir, 'archiware_p5_jobs.json'), 'w') as fp:
