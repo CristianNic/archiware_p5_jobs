8191459933b99aa0c8c1d1230367f757e6e21eef
CristianNic <cristian.niculescu@gmail.com>
2020-08-01 23:14:10 -0700

    rename js function

--- js/archiware_p5_jobs.js
+++ js/archiware_p5_jobs.js
@@ -1,14 +1,19 @@
 
 
-var formatNewLines = function ( col, row ) {
+var formatArchiware_p5NewLines = function ( col, row ) {
   // Get cell data and cell position, and make it into variables `cell` and `value`
   alert("here");
-  var cell = $('td:eq('+col+')', row);
-  var value = cell.text();
+  var cell = $('td:eq('+col+')', row),
+      value = cell.text();
   // Process contents of variable `value`
-  var newValue = value.replace(/\n/g, "<br>").replace(/\r/g, "<br>");
+  value = value.replace(/\n/g, "<br>").replace(/\r/g, "<br>");
   // Output new variable `value` to cell
-  cell.html(newValue)
+  cell.html(value)
 }
+
+
+
+
+
 
 
