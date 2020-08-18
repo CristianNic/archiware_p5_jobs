39d2f32ca3204961f7fcaefe39fa90044e5da5db
CristianNic <cristian.niculescu@gmail.com>
2020-08-18 14:18:23 -0700

    json decode priority

--- archiware_p5_jobs_processor.php
+++ archiware_p5_jobs_processor.php
@@ -6,13 +6,10 @@ class Archiware_p5_jobs_processor extends Processor
 {
     public function run($json)
     {
-        // Check if data was uploaded
         if (! $json ) {
             throw new Exception("Error Processing Request: No JSON file found", 1);
         }
-        // Process json into object thingy
         $data = json_decode($json, true);
-        $data['serial_number'] = $this->serial_number;
         Archiware_p5_jobs_model::where('serial_number', $this->serial_number)->delete();
         foreach($data as $key => $job ) {
             $data[$key]['serial_number'] = $this->serial_number;
@@ -20,5 +17,3 @@ class Archiware_p5_jobs_processor extends Processor
         Archiware_p5_jobs_model::insertChunked($data);
     }
 }
-
-
